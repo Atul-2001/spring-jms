f8ab03799d9ce08b7481c3c3c0e4c05464491270
Atendra Suryvanshi <suryavanshi.atendra15@gmail.com>
2022-10-11 17:59:19 +0530

    adding send and receive example

--- src/main/java/com/signature/listener/MessageListener.java
+++ src/main/java/com/signature/listener/MessageListener.java
@@ -3,21 +3,40 @@ package com.signature.listener;
 import com.signature.config.JmsConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class MessageListener {
+
+    private final JmsTemplate jmsTemplate;
+
+    public MessageListener(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload final com.signature.model.Message payload,
                        @Headers MessageHeaders messageHeaders, Message message) {
         log.info("I got a message!!!");
         log.info(payload.toString());
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload final com.signature.model.Message payload,
+                               @Headers MessageHeaders messageHeaders, Message message) throws JMSException {
+        var replyMessage = com.signature.model.Message.builder()
+                .id(UUID.randomUUID()).message("World!").build();
+
+        log.info("I got a message!!!, sending reply");
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), replyMessage);
     }
 }
--- src/main/java/com/signature/sender/MessageSender.java
+++ src/main/java/com/signature/sender/MessageSender.java
@@ -1,5 +1,6 @@
 package com.signature.sender;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.signature.config.JmsConfig;
 import com.signature.model.Message;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,7 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import java.util.UUID;
 
 @Slf4j
@@ -14,9 +16,12 @@ import java.util.UUID;
 public class MessageSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
-    public MessageSender(JmsTemplate jmsTemplate) {
+    public MessageSender(JmsTemplate jmsTemplate,
+                         ObjectMapper objectMapper) {
         this.jmsTemplate = jmsTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @Scheduled(fixedRate = 2000)
@@ -30,5 +35,25 @@ public class MessageSender {
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
         log.info("Message sent.");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+        final Message message = Message
+                .builder().id(UUID.randomUUID())
+                .message("Hello!").build();
+
+        var receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, session -> {
+            try {
+                var textMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                textMessage.setStringProperty("_type", Message.class.getCanonicalName());
+                log.info("Sending message: " + textMessage.getText());
+                return textMessage;
+            } catch (final Exception ex) {
+                throw new JMSException(ex.getMessage());
+            }
+        });
+
+        log.info("Received message: " + receivedMsg.getBody(String.class));
     }
 }
